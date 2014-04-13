161a96ed06946187c2fd72b794da4cb9b943f85a
nyappyinjapan <nw0345@gmail.com>
2014-04-13 11:13:37 +0900

    画面サイズに背景を合わせる（調整中）

--- public/main/js/BackGround.js
+++ public/main/js/BackGround.js
@@ -2,25 +2,26 @@ function BackGround (bgfilename, mvlength, childlayer) {
 	this.bgfilename = bgfilename;
 	this.mvlength = mvlength;
 	this.childlayer = childlayer;
-	this.imagewidth = 200;
+	this.imagewidth = 200/2;
 	this.center = {x1:100, x2:300, y:200 };
     this.back = tm.display.Sprite("star", 200, 200).addChildTo(childlayer);
     this.back2 = tm.display.Sprite("star", 200, 200).addChildTo(childlayer);
     this.back.setPosition(this.center.x1,this.center.y);
     this.back2.setPosition(this.center.x2,this.center.y);
-    this.WIDTH = 600;
+    this.WIDTH = window.innerWidth;
 }
 
 BackGround.prototype = {
 
 	update: function() {
 		this.center.x1 += this.mvlength;
-		if(this.center.x1 > 700) this.center.x1 = this.imagewidth/2;
-		if(this.center.x1 <-100) this.center.x1 = this.WIDTH;
+		if(this.center.x1 > this.WIDTH) this.center.x1 = this.imagewidth;
+		if(this.center.x1 < -this.imagewidth) this.center.x1 = this.WIDTH-this.imagewidth*2;
 		this.back.setPosition(this.center.x1,this.center.y);
+
 		this.center.x2 += this.mvlength;
-		if(this.center.x2 > 700) this.center.x2 = this.imagewidth/2;
-		if(this.center.x2 <-100) this.center.x2 = this.WIDTH;
+		if(this.center.x2 > this.WIDTH) this.center.x2 = this.imagewidth;
+		if(this.center.x2 < -this.imagewidth) this.center.x2 = this.WIDTH-this.imagewidth*2;
 	    this.back2.setPosition(this.center.x2,this.center.y);
 	},
 
